56ca43e5524f14d9cb495f82b320b873da38e390
codewithnisha23 <cs.nishathakur@gmail.com>
2026-03-22 10:15:49 +0530

    Removed heavy files

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9S3OGgjiqdU3dGFrhpXCLoRPLH6WkhIdUKOZzghzUnw=\"\n}"
--- .next/dev/static/chunks/app_contact_page_tsx_a8c03fea._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_4c6525e2._.js"
-],
-    source: "dynamic"
-});
